3cd3c9f3e909acd35034ec63b85946a2d2a8b620
Raghav Kumar <raghkum2000@gmail.com>
2024-04-11 08:18:21 +0530

    made project card grid and added description

--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -16,7 +16,7 @@ const ProjectNames = [
     "College Scheduler",
     "MLSC Cryptic Hunt Portal",
     "Intel OneAPI Code Maven Hackathon",
-    "Blue Yonder Demand and Supply Problem",
+    "Blue Yonder Demand and Supply Hackathon",
 ] as const 
 
 type TProject = typeof ProjectNames[number]
@@ -27,10 +27,33 @@ const ProjectComponent:Map<TProject, React.FC> = new Map([
     ["College Scheduler", CollegeScheduler],
     ["MLSC Cryptic Hunt Portal", MLSCHuntPortal],
     ["Intel OneAPI Code Maven Hackathon", IntelHackathon],
-    ["Blue Yonder Demand and Supply Problem", BlueYonderHackathon],
+    ["Blue Yonder Demand and Supply Hackathon", BlueYonderHackathon],
     ["PowerBot: MAQ", PowerBot]
 ])
-
+const ProjectDescription:Map<TProject, string> = new Map([
+    [
+        "PowerBot: MAQ", 
+        "My experience as a Data Engineer/Software Engineer at MAQ Software."
+    ],[
+        "Event Analysis", 
+        "A CUDA accelerated scientific computation library used for detecting extreme weather events."
+    ],[
+        "Flight Simulator", 
+        "An OpenGL based flight simulator in C++ with no external dependencies."
+    ],[
+        "College Scheduler", 
+        "My Software Engineering course project that allows for creating a college schedule given the requirements."
+    ],[
+        "MLSC Cryptic Hunt Portal", 
+        "An portal created and used for an online cryptic hunt."
+    ],[
+        "Intel OneAPI Code Maven Hackathon", 
+        "An Intel OneAPI solution to predict stocks."
+    ],[
+        "Blue Yonder Demand and Supply Hackathon", 
+        "A Monte Carlo Tree Search implementation to optimise the logistics problem of optimal trucking."
+    ]
+])
 const uniqueRand = (min:number, max:number, prev:number) => {
     let next = prev;
     
@@ -76,6 +99,7 @@ const Projects:React.FC<{
     const PauseEffectEvent = () => setPauseEffect(true);
     const ResumeEffectEvent = () => {
         if(selectedProject) return ;
+        return ;
         setPauseEffect(false);
     };
     const ProjectContent =  ProjectComponent.get(selectedProject!) ?? null
@@ -94,15 +118,25 @@ const Projects:React.FC<{
                 id="living-shapes-wrapper"
                 onMouseEnter={PauseEffectEvent} 
                 onMouseLeave={ResumeEffectEvent} 
+                onTouchStart={PauseEffectEvent}
             >
 
 
                 {ProjectNames.map(project_name => 
-                    <div className="shape cursor-pointer" data-selected={selectedProject === project_name} >
-                        <p onClick={()=>{
+                    <div 
+                        className="shape cursor-pointer" 
+                        data-selected={selectedProject === project_name}
+                        onClick={()=>{
                             setSelectedProject(project_name);
-                        }} className=''>
-                            {project_name}
+                        }} 
+                    >
+                        <p className=''>
+                            <p>
+                                {project_name}
+                            </p>
+                            <p className='text-xs font-normal	'>
+                                {ProjectDescription.get(project_name)!}
+                            </p>
                         </p>
                         {ProjectContent && <ProjectContent />}
                     </div>
